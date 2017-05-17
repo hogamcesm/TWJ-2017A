9ba1e5d1ca75310bea50e21c9687ce0a83598418
hogamcesm <USRDEL@USR4.local>
2017-05-17 14:33:54 -0500

    Angular 2

--- 4. Angular/Web01/src/app/app.component.ts
+++ 4. Angular/Web01/src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'twj-raiz',
+  template: "<p>Hola Mundo</p>"
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello World!!!!!!!!!!!!!!';
-  nombre = "Carlos";
+  usuario: UsuarioInt = {
+    nombre: "Carlos",
+    apellido: "Sampedro"
+  };
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    console.log(this.saludar(this.usuario.nombre,
+      this.usuario.apellido));
+  }
+
+  holaMundo(){
+    console.log("Hola Mundo");
+  }
+  saludar(nombre:string, apellido?:string):string{
+    return `Hola ${nombre} ${apellido}!`;
+  }
+}
+
+interface UsuarioInt {
+  nombre: string,
+  apellido: string
 }
