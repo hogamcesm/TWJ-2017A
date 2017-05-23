7daa4b1bb493ff98f5c761104bfd59b8f00ee724
hogamcesm <USRDEL@USR4.local>
2017-05-23 16:59:30 -0500

    Binding y css

--- 4. Angular/Web01/src/app/app.component.ts
+++ 4. Angular/Web01/src/app/app.component.ts
@@ -8,27 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   usuario: UsuarioInt = {
-    nombre: "Carlos",
+    nombre: "",
     apellido: "Sampedro"
   };
 
   url:string = "https://angular.io";
+  img:string="https://d2slcw3kip6qmk.cloudfront.net/marketing/pages/chart/images/common/persona-icons-itengineering@2x.jpg";
+  img2:string="http://static.bandainamcogames.eu/sites_products/onepiece/uploads/2016/03/home-logo_en-1-1.png";
+  parrafo = "asdsadasdasdsadas";
+  background: string = "lightgreen";
+  width: string = "80px";
+  height: string = "70px";
+
+
+
+
   constructor(){
-    console.log("Constructor");
-    this.holaMundo();
-    console.log(this.saludar(this.usuario.nombre,
-      this.usuario.apellido));
+    //console.log("Constructor");
+    //this.holaMundo();
+    //console.log(this.saludar(this.usuario.nombre,
+     // this.usuario.apellido));
+    this.usuario.nombre = "Carlos";
+    setTimeout(()=>{
+      this.usuario.nombre = "Eduardo"
+    },3000)
   }
 
   holaMundo(){
     console.log("Hola Mundo");
   }
-  saludar(nombre:string, apellido?:string):string{
+  saludar( nombre:string, apellido?:string):string{
     return `Hola ${nombre} ${apellido}!`;
   }
 }
 
 interface UsuarioInt {
-  nombre: string,
+  nombre: string ,
   apellido: string
 }
