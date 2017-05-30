9086899ceeb7ac45bba447172ff3eea4a6804eac
hogamcesm <USRDEL@USR4.local>
2017-05-30 16:58:14 -0500

    Clase

--- 4. Angular/Web01/src/app/componentes/inicio/inicio.component.ts
+++ 4. Angular/Web01/src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  nombre: string = "Carlos";
 
-  constructor() { }
+  /* command + alt, seleccionar todo el texto, command + alt + l*/
+  arregloUsuarios = [{
+    nombre: "Carlos",
+    apellido: "Sampedro",
+    conectado: "true"
+  }, {
+    nombre: "Mashi",
+    apellido: "Correa",
+    conectado: "true"
+  }, {
+    nombre: "Abdala",
+    apellido: "Bucaram",
+    conectado: "true"
+  }];
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+  }
+
+  cambiarNombre(): void {
+
+    console.log("Entro");
+    this.nombre = "Rafico a Lenin";
+  }
+
+  cambiarOtroNombre() {
+    this.nombre = "Lenin a Rafico";
+  }
+
+  cambiarNombreInput(nombreEtiqueta) {
+    console.log(nombreEtiqueta.type);
+    console.log(nombreEtiqueta.placeholder);
+    console.log(nombreEtiqueta.value);
+
+    this.nombre = nombreEtiqueta.value;
+
+
+  }
 }
